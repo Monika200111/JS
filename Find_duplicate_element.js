02d3c952dd6131799b987cc1573c9f6c5baf59a8
Monika Sharma <ms2349161@gmail.com>
2026-09-23 11:08:32 +0530

    Create Find_duplicate_element

---
+++ Find_duplicate_element.js
@@ -0,0 +1,15 @@
+let a = [1, 2, 3, 2, 4, 5, 3,4]
+
+let b = []
+
+let c = []
+
+for(let i =0; i<a.length; i++){
+    if(b.includes(a[i])){
+      c.push(a[i])
+    }
+    else{
+      b.push(a[i])
+    }
+}
+console.log(c)
